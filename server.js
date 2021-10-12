0ebad43b43a94923e0b4a53ccf9044e3c8ac880d
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-12 16:04:33 -0400

    api get test

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const cors = require('cors')
 
 const bodyParser = require('body-parser');
 
+var mongoose = require("mongoose");
+const { json } = require('body-parser');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+  username: String,
+  _id: String
+});
+
+let User = mongoose.model("User", userSchema);
 
 require('dotenv').config()
 
@@ -22,12 +33,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', function(req, res)
+{
+  res.send(['swag', 'also swagged']);
+});
+
 
 app.post("/api/users", function(req, res)
 {
   console.log(req.body);
-  res.json({username: req.body.username,  _id: uuidv4()});
-})
+
+  var jsonShit = {username: req.body.username,  _id: uuidv4()};
+
+  User.create(jsonShit, function (err, user)
+  {
+    if (err) return console.log(err);
+
+    res.json(jsonShit);
+  });
+
+  
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
