7411a666a8570e879426dd491810a76f9ea8b1b3
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-12 15:31:37 -0400

    initial post shit

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+const bodyParser = require('body-parser');
+
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
-
+app.post("/api/users", function(res, req)
+{
+  console.log(req.body);
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
